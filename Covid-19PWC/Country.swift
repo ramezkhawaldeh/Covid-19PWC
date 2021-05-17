d8ca41096e70e70e1fdd0f6abc88416629b9671d
RamezKhawaldeh <ramezkhawaldeh@outlook.com>
2021-05-17 11:58:39 +0300

    initial commit (added Country Data model)

---
+++ Covid-19PWC/Country.swift
@@ -0,0 +1,15 @@
+//
+//  Country.swift
+//  Covid-19PWC
+//
+//  Created by Ramez Khawaldeh on 17/05/2021.
+//
+
+import Foundation
+
+struct Country: Codable {
+    let flag: String
+    let name: String
+    let alpha3Code: String
+    
+}
--- Covid-19PWC/ViewController.swift
+++ Covid-19PWC/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var textField: UITextField!
+    
+    var country: Country?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func getDataButtonPressed(_ sender: Any) {
+        guard let country = textField.text else { return }
+        getData(with: country)
+    }
 }
 
+extension ViewController {
+    
+    private func getData(with country: String) {
+        if let url = URL(string: "https://restcountries.eu/rest/v2/name/\(country)?fields=name;flag;alpha3Code") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                guard let data = data, error == nil else {
+                    print(error?.localizedDescription)
+                    return
+                }
+                self.country = self.parseJSON(with: data)
+            }
+            task.resume()
+        }
+    }
+    
+    private func parseJSON(with data: Data) -> Country? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode([Country].self, from: data)
+            return decodedData.first
+        }
+        catch {
+            print(error)
+            return nil
+        }
+    }
+}
